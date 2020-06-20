47afdd83a3c03aacb3a73d3a34b5703233bc3065
XCarrel <Xavier.Carrel@cpnv.ch>
2020-06-20 10:59:30 +0200

    Fixed bug of IsInPolygon

--- Krohonde/Helpers.cs
+++ Krohonde/Helpers.cs
@@ -18,6 +18,9 @@ namespace Krohonde
         }
 
         // Thanks to https://stackoverflow.com/questions/4243042/c-sharp-point-in-polygon
+        // With two bugs corrected:
+        // 1) Point on the line of a segment, but far from the polygon was considered in. Removed the test if (coef.Any(p => p == 0))
+        // 2) With large coordinates, the product of two consecutive coefs produced a wrong signed value because of overflow. Adde Math.Sign()
         public static bool IsInPolygon(Point[] poly, Point point)
         {
             var coef = poly.Skip(1).Select((p, i) =>
@@ -25,12 +28,9 @@ namespace Krohonde
                                           - (point.X - poly[i].X) * (p.Y - poly[i].Y))
                                     .ToList();
 
-            if (coef.Any(p => p == 0))
-                return true;
-
             for (int i = 1; i < coef.Count(); i++)
             {
-                if (coef[i] * coef[i - 1] < 0)
+                if (Math.Sign(coef[i]) * Math.Sign(coef[i - 1]) < 0)
                     return false;
             }
             return true;
