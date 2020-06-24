a0908ab7bb2c2fde8fceaf126edd6403c5fa4567
Andolfatto <frederique.andolfatto@cpnv.ch>
2020-06-24 09:04:54 +0200

    add Score

---
+++ FormsApp/Score.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Krohonde;
+using Krohonde.RedColony;
+using Krohonde.GreenColony;
+using System.Diagnostics;
+
+namespace FormsApp
+{
+    public partial class Score : Form
+    {
+        private const int SCORE_GRP_X = 35;
+        private const int SCORE_GRP_Y = 80;
+        private const int SCORE_GRP_WIDTH = 200;
+        private const int SCORE_GRP_HEIGHT = 75;
+
+        private List<Colony> colonies;
+        private Stopwatch stopWatch;
+        private System.Windows.Forms.GroupBox grpColonie;
+        private System.Windows.Forms.Label lblAntNb;
+
+        public List<Colony> Colonies
+        {
+            get
+            {
+                return colonies;
+            }
+            set
+            {
+                colonies = value;
+            }
+        }
+
+        public Stopwatch StopWatch
+        {
+            get
+            {
+                return stopWatch;
+            }
+            set
+            {
+                stopWatch = value;
+            }
+        }
+
+        public Score()
+        {
+            InitializeComponent();
+        }
+
+         
+        private void Score_Load(object sender, EventArgs e)
+        {
+            int nbColony = 0;
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = "";
+            bool hourEmpty = true;
+
+            if (ts.Hours > 0)
+            {
+                elapsedTime += String.Format("{0:00} h", ts.Hours);
+                hourEmpty = false;
+            }
+            if (ts.Minutes > 0)
+            {
+                if (!hourEmpty)
+                    elapsedTime += " ";
+                elapsedTime += String.Format("{0:00} min", ts.Minutes);
+                hourEmpty = false;
+            }
+            if (ts.Seconds > 0)
+            {
+                if (!hourEmpty)
+                    elapsedTime += " ";
+                elapsedTime += String.Format("{0:00} s", ts.Seconds);
+            }
+
+            this.Height = 0;
+
+            lblElapsedTime.Text = lblElapsedTime.Text + " : " + elapsedTime;
+
+            foreach (Colony colony in colonies)
+            {
+                this.grpColonie = new System.Windows.Forms.GroupBox();
+                this.lblAntNb = new System.Windows.Forms.Label();
+                this.grpColonie.SuspendLayout();
+                // 
+                // grpColonie
+                // 
+                this.grpColonie.Controls.Add(this.lblAntNb);
+                if (nbColony > 0)
+                {
+                    this.grpColonie.Location = new System.Drawing.Point(SCORE_GRP_X, SCORE_GRP_Y + nbColony * (SCORE_GRP_HEIGHT + 10));
+                } else
+                {
+                    this.grpColonie.Location = new System.Drawing.Point(SCORE_GRP_X, SCORE_GRP_Y);
+                }
+                this.grpColonie.Name = "grpColonie";
+                this.grpColonie.Size = new System.Drawing.Size(SCORE_GRP_WIDTH, SCORE_GRP_HEIGHT);
+                this.grpColonie.TabIndex = 1;
+                this.grpColonie.TabStop = false;
+                this.grpColonie.Text = "Colonie " + colony.Color.ToKnownColor().ToString();
+                // 
+                // lblAntNb
+                // 
+                this.lblAntNb.AutoSize = true;
+                this.lblAntNb.Location = new System.Drawing.Point(25, 35);
+                this.lblAntNb.Name = "lblAntNb";
+                this.lblAntNb.Size = new System.Drawing.Size(128, 17);
+                this.lblAntNb.TabIndex = 0;
+                this.lblAntNb.Text = "Nombre de fourmis : " + colony.Population.Count;
+
+                this.Controls.Add(this.grpColonie);
+                this.grpColonie.ResumeLayout(false);
+                this.grpColonie.PerformLayout();
+
+                nbColony++;
+
+                int taille = 50 + SCORE_GRP_Y + (nbColony * (SCORE_GRP_HEIGHT + 10));
+
+                this.Height = taille;
+                
+                
+            }
+        }
+    }
+}
---
+++ FormsApp/Score.designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FormsApp
+{
+    partial class Score
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblElapsedTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblElapsedTime
+            // 
+            this.lblElapsedTime.AutoSize = true;
+            this.lblElapsedTime.Location = new System.Drawing.Point(46, 35);
+            this.lblElapsedTime.Name = "lblElapsedTime";
+            this.lblElapsedTime.Size = new System.Drawing.Size(97, 17);
+            this.lblElapsedTime.TabIndex = 0;
+            this.lblElapsedTime.Text = "Temps écoulé";
+            // 
+            // Score
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FormsApp.Properties.Resources.grass;
+            this.ClientSize = new System.Drawing.Size(438, 387);
+            this.Controls.Add(this.lblElapsedTime);
+            this.Name = "Score";
+            this.Text = "Score";
+            this.Load += new System.EventHandler(this.Score_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblElapsedTime;
+        
+    }
+}
