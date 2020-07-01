f88d8c9f5040615e6ab476bbe62fa9a4fb21b39d
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-07-01 16:56:00 +0200

    Implemented basic Behaviour

--- GreenColony/FarmerAnt.cs
+++ GreenColony/FarmerAnt.cs
@@ -15,6 +15,64 @@ namespace Krohonde.GreenColony
 
         public override void Live(double deltatime)
         {
+            //Updates food list
+            //Todo:update interval for energy efficiency
+            List<Food> greenFoods = FoodAroundMe();
+            //default speed direction
+            //Todo:change default
+            Speed.X = -20;
+
+            //when food is spotted
+            if (greenFoods.Count > 0)
+            {
+                Food closestFood = greenFoods[0];
+                foreach (Food f in greenFoods)
+                {
+                    Logger.WriteLogFile($"GreenFarmer found food at x:{f.Location.X} ; y:{f.Location.Y}");
+
+                    //Check which on is closer
+                    if (Helpers.Distance(f.Location, this.SDLocation) <
+                        Helpers.Distance(closestFood.Location, this.SDLocation))
+                    {
+                        closestFood = f;
+                    }
+                }
+                
+                //go toward the closes food
+                //todo:add distance to line function in order to match the 0
+                if (X < closestFood.Location.X)
+                {
+                    Speed.X = +20;
+                }
+                else if (X > closestFood.Location.X)
+                {
+                    Speed.X = -20;
+                }
+                else
+                {
+                    Speed.X = 0;
+                }
+                
+                if (Y < closestFood.Location.Y)
+                {
+                    Speed.Y = +20;
+                }
+                else if (Y > closestFood.Location.Y)
+                {
+                    Speed.Y = -20;
+                }
+                else
+                {
+                    Speed.Y = 0;
+                }
+                
+                //Todo: update function
+                if (Helpers.Distance(closestFood.Location, SDLocation) < PICKUP_REACH)
+                {
+                    Pickup(closestFood);
+                }
+            }
+
             Move(deltatime);
         }
     }
