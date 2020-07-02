eef62b2bd35b221e4c9ba450b7e23ff7df878e10
SebastienMoraz <sebastien.moraz@cpnv.ch>
2020-07-02 16:51:02 +0200

    push

--- RedColony/WorkerAnt.cs
+++ RedColony/WorkerAnt.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
+/*
+ * Juste pour avoir le dernier commit
+ */
 namespace Krohonde.RedColony
 {
     public class WorkerAnt : Ant
