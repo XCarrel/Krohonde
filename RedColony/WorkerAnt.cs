203294c61539709b38df2f2a3b7f13bda2bd5750
SebastienMoraz <sebastien.moraz@cpnv.ch>
2020-07-02 14:54:20 +0200

    maj worker red parce que manger, ca sert a rien

--- RedColony/WorkerAnt.cs
+++ RedColony/WorkerAnt.cs
@@ -12,6 +12,7 @@ namespace Krohonde.RedColony
         private Point position;
         private Point goToPosition;
         int inc = 0;
+        int nouriture = 0;
         
         
 
@@ -23,63 +24,6 @@ namespace Krohonde.RedColony
         public override void Live()
         {
             double distMin = 5000;
-
-            if ( Energy < 28000)
-            {
-
-                if(FoodBag < 1)
-                {
-                    List<Food> foodposition = FoodAroundMe();
-                    if (foodposition.Count() > 0)
-                    {
-                        Food closest = foodposition[0];
-                        foreach (Food foodproche in foodposition)   //find zone
-                        {
-                            if (Helpers.Distance(SDLocation, foodproche.Location) < distMin)
-                            {
-                                closest = foodproche;
-                                distMin = Helpers.Distance(SDLocation, foodproche.Location);
-                            }
-                        }
-                        int xPos = closest.Location.X;
-                        int yPos = closest.Location.Y;
-                        int xMyPos = Convert.ToInt32(X);
-                        int yMyPos = Convert.ToInt32(Y);
-                        float distance = Math.Abs(xPos - xMyPos) + Math.Abs(yPos - yMyPos);
-
-                        if (distance < 2.0f)
-                        {
-                            ScoutAnt.DesactivateRessource(closest);
-                            Pickup(closest);
-                        }
-                        else
-                        {
-                            Speed.X = closest.Location.X - X;
-                            Speed.Y = closest.Location.Y - Y;
-                            Move();
-                        }
-                    }
-                    else
-                    {
-                        Resource procheSelonScouts = ScoutAnt.GoToResource(new Point(X, Y), true, true);
-
-                        if (procheSelonScouts != null)
-                        {
-                            goToPosition = new Point(procheSelonScouts.Location.X, procheSelonScouts.Location.Y);
-                        }
-
-
-                        Speed.X = goToPosition.X - X;
-                        Speed.Y = goToPosition.Y - Y;
-                        Move();
-                    }
-                }
-
-                
-            }
-
-
-
 
             inc++;
             if(inc == 3)
