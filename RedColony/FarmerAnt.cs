98a69714eace2dcddd71d266c7a1b82f5efe5277
Juillet-Meline <61775676+Juillet-Meline@users.noreply.github.com>
2020-07-02 09:45:45 +0200

    drop farmer red

--- RedColony/FarmerAnt.cs
+++ RedColony/FarmerAnt.cs
@@ -13,6 +13,7 @@ namespace Krohonde.RedColony
         private Point goToPosition;
         double startSpawnX = 0;
         double startSpawnY = 0;
+        bool canDumpFood = false;
 
         public FarmerAnt(Point location, Point speed, RedColony colony) : base(location, speed, colony)
         {
@@ -24,62 +25,83 @@ namespace Krohonde.RedColony
         {
 
             if (FoodBag >48) {
-                Speed.X = startSpawnX - X;
-                Speed.Y = startSpawnY - Y;
-                Move();
-            }
-            
-
-            double disMin = 5000;
-
-            
-
-            List<Food> foodposition = FoodAroundMe();
-            if (foodposition.Count() > 0)
-            {
-                Food closest = foodposition[0];
-                foreach (Food foodproche in foodposition)
+                if (X != startSpawnX && Y != startSpawnY)
                 {
-                    if (Helpers.Distance (SDLocation, foodproche.Location)< disMin)
-                    {
-                        closest = foodproche;
-                        disMin = Helpers.Distance(SDLocation, foodproche.Location);
-                    }
-                }
-                int xPos = closest.Location.X;
-                int yPos = closest.Location.Y;
-                int xMyPos = Convert.ToInt32(X);
-                int yMyPos = Convert.ToInt32(Y);
-                float distance = Math.Abs(xPos - xMyPos) + Math.Abs(yPos - yMyPos);
-
-                if (distance < 2.0f)
-                {
-                    ScoutAnt.DesactivateRessource(closest);
-                    Pickup(closest);
-                }
-                else
-                {
-                    Speed.X = closest.Location.X - X;
-                    Speed.Y = closest.Location.Y - Y;
+                    Speed.X = startSpawnX - X;
+                    Speed.Y = startSpawnY - Y;
                     Move();
+
+                    
+                }
+                else {
+                    canDumpFood = true;
+                }
+
+            }
+            if (canDumpFood)
+            {
+                MyColony.DumpFood(this);
+                if (FoodBag == 0)
+                {
+                    canDumpFood = false;
                 }
             }
             else
             {
-                Resource procheSelonScouts = ScoutAnt.GoToResource(new Point(X, Y), true, true);
 
-                if (procheSelonScouts != null)
+
+
+
+                double disMin = 5000;
+
+
+
+                List<Food> foodposition = FoodAroundMe();
+                if (foodposition.Count() > 0)
                 {
-                    goToPosition = new Point(procheSelonScouts.Location.X, procheSelonScouts.Location.Y);
+                    Food closest = foodposition[0];
+                    foreach (Food foodproche in foodposition)
+                    {
+                        if (Helpers.Distance(SDLocation, foodproche.Location) < disMin)
+                        {
+                            closest = foodproche;
+                            disMin = Helpers.Distance(SDLocation, foodproche.Location);
+                        }
+                    }
+                    int xPos = closest.Location.X;
+                    int yPos = closest.Location.Y;
+                    int xMyPos = Convert.ToInt32(X);
+                    int yMyPos = Convert.ToInt32(Y);
+                    float distance = Math.Abs(xPos - xMyPos) + Math.Abs(yPos - yMyPos);
+
+                    if (distance < 2.0f)
+                    {
+                        ScoutAnt.DesactivateRessource(closest);
+                        Pickup(closest);
+                    }
+                    else
+                    {
+                        Speed.X = closest.Location.X - X;
+                        Speed.Y = closest.Location.Y - Y;
+                        Move();
+                    }
+                }
+                else
+                {
+                    Resource procheSelonScouts = ScoutAnt.GoToResource(new Point(X, Y), true, true);
+
+                    if (procheSelonScouts != null)
+                    {
+                        goToPosition = new Point(procheSelonScouts.Location.X, procheSelonScouts.Location.Y);
+                    }
+
+
+                    Speed.X = goToPosition.X - X;
+                    Speed.Y = goToPosition.Y - Y;
+                    Move();
                 }
 
-
-                Speed.X = goToPosition.X - X;
-                Speed.Y = goToPosition.Y - Y;
-                Move();
             }
-
-
             
             
 
