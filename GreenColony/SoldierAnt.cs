d761fcd933552595bb836b1e559a9c5c80ebc476
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-07-02 14:38:16 +0200

    Target ennemies + follow Farmerant + base si pas farmerant + fight si soldierant ennemy

--- GreenColony/SoldierAnt.cs
+++ GreenColony/SoldierAnt.cs
@@ -9,21 +9,92 @@ namespace Krohonde.GreenColony
 {
     public class SoldierAnt : Ant
     {
+        Ant followAnt; // La fourmis qu'on escorte
         public SoldierAnt(Point location, Point speed, GreenColony colony) : base(location, speed, colony)
         {
+
         }
 
         public override void Live()
         {
-            Speed.X = 10;
-            Speed.Y = -10;
+            followAnt = MyColony.Population.First();
+            MoveToward(followAnt.SDLocation);
             Move();
 
-            /*while(MotherNature.PheromonTypes.Danger))
+
+            
+            foreach(Ant ant in MyColony.Population)
+            {
+                if (ant.GetType().Name == "FarmerAnt")
+                {
+                    followAnt = ant;
+                    break;
+                }
+                else
+                {
+                    MoveToward(new System.Drawing.Point((int) MyColony.Location.X, (int) MyColony.Location.Y));
+                   
+                }
+            }
+
+
+            List<Ant> Ennemies = EnemiesAroundMe();
+            foreach(Ant Ennemy in Ennemies)
+            {
+                if (Ennemy.GetType().Name == "SoldierAnt")
+                {
+                    if(Helpers.Distance(Ennemy.SDLocation, SDLocation) <= HIT_REACH)
+                    {
+                        Hit(Ennemy);
+                    }
+                    else
+                    {
+                        MoveToward(Ennemy.SDLocation);
+                    }
+                }
+            }
+
+            /*List<Pheromon> ListPheromon = SmellsAroundMe();
+            
+            foreach (Pheromon AllPheromon in ListPheromon)
             {
 
             }
-            */
+
+           
+            while (MotherNature.PheromonTypes.Danger >= 1))
+            {
+                
+            }*/
+
+        }
+        private void MoveToward(System.Drawing.Point targetLocation)
+        {
+            //calculates the distance
+            var distanceToX = targetLocation.X - X;
+            var distanceToY = targetLocation.Y - Y;
+            //makes distances positive values
+            if (distanceToX < 0) distanceToX *= -1;
+            if (distanceToY < 0) distanceToY *= -1;
+
+            //adjust signs
+            if (X <= targetLocation.X)
+            {
+                Speed.X = distanceToX;
+            }
+            else
+            {
+                Speed.X = -distanceToX;
+            }
+
+            if (Y <= targetLocation.Y)
+            {
+                Speed.Y = +distanceToY;
+            }
+            else
+            {
+                Speed.Y = -distanceToY;
+            }
         }
     }
 }
