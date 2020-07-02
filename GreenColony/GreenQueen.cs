1caa1c69a3ff438d3201e22bc7ec122f16be3127
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-07-02 16:40:05 +0200

    Spawn de soldats en force

--- GreenColony/GreenQueen.cs
+++ GreenColony/GreenQueen.cs
@@ -7,15 +7,27 @@ using System.Drawing;
 
 namespace Krohonde.GreenColony
 {
-    public class GreenQueen: Queen
+    public class GreenQueen : Queen
     {
         public GreenQueen(Point location, Point speed, Colony colony) : base(location, speed, colony)
         { }
         public override void Live()
         {
             base.Live();
+            for (int i = 0; i < 30; i++)
+            {
+                if(Energy <= 20000)
+                {
+                    Eat(i);
+                }
+                else
+                {
+                    LayEgg(MotherNature.AntTypes.SoldierAnt, new Point((int)X + 10, (int)Y));
+                }
+                
+            }
             DoNothing(); // The queen MUST either do something (Move, Eat, Lay an egg) or announce that she does nothing
         }
-
+        
     }
 }
